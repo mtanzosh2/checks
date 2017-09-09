8d4ea43853371f9d89cdedbaf9931beaaa6087d3
Margaret Tanzosh <margarettanzosh@nestmk12.net>
2017-09-09 15:25:51 +0000

    added wordcount

--- discomfort/check50/__init__.py
+++ discomfort/check50/__init__.py
@@ -7,3 +7,11 @@ class Discomfort(Checks):
     def submitted(self):
         """ "What is your discomfort?" submitted"""
         pass
+
+    @check()
+    def at_least_50_words(self):
+        """assignment has at least 50 words"""
+        with open("assignment.txt") as f:
+            word_count = len(f.read().split())
+            if word_count < 50:
+                raise Error("50 words required, only {} found.".format(word_count))
