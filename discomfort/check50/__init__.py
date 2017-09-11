53d3040dd410867b47bfb4c952026b2e9e67affa
Margaret Tanzosh <margarettanzosh@nestmk12.net>
2017-09-11 19:10:18 -0400

    Update __init__.py

--- discomfort/check50/__init__.py
+++ discomfort/check50/__init__.py
@@ -4,13 +4,7 @@ class Discomfort(Checks):
 
     @check()
     def submitted(self):
-        """discomfort.txt exists"""
-        pass
-
-    @check()
-    def at_least_200_words(self):
-        """Assignment has approx 300 to 500 words"""
-        with open("discomfort.txt") as f:
-            word_count = len(f.read().split())
-            if word_count < 200:
-                raise Error("300 to 500 words required, only {} found.".format(word_count))
+        """You submitted 'What is my discomfort'"""
+        files = os.listdir()
+        if not any(filename.startswith("discomfort") for filename in files):
+            raise Error("File not found")
